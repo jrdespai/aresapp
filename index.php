f539cbe2ff77cf069c7adfe27dece9ff259afae9
jrdespai <jrdespai@asu.edu>
2015-03-11 17:35:41 -0700

    Initial commit of php files

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <!-- Meta tag -->
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+	<!-- Bootstrap Reference -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	
+    <!-- Web Page Title -->
+    <title>Ares App</title>
+	
+	<!--Image retreived from: http://www.seeyalater.org/wp-content/uploads/22526.jpg-->
+	<link rel="icon" href="images/football.png" />
+	
+  </head>
+  <body>
+  
+
+
+<form role="form" action="login.php" method="post">
+	<div class="form-group">
+		<label for="username">Username:</label>
+		<input type="text" class="form-control" id="username" name="username">
+	</div>
+	<div class="form-group">
+		<label for="password">Password:</label>
+		<input type="password" class="form-control" id="password" name="password">
+	</div>
+	<button type="submit" class="btn btn-default">Login</button>
+</form>
+
+
+  
+  </body>
---
+++ login.php
@@ -0,0 +1,49 @@
+<html lang="en">
+  <head>
+    <!-- Meta tag -->
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+	<!-- Bootstrap Reference -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	
+    <!-- Web Page Title -->
+    <title>Ares App</title>
+	
+	<!--Image retreived from: http://www.seeyalater.org/wp-content/uploads/22526.jpg-->
+	<link rel="icon" href="images/football.png" />
+	
+  </head>
+  <body>
+<?php
+	//local-connect.php
+	
+	$hostName 	= 'localhost';
+	$userName	= 'root';
+	$pass		= '';
+	$database	= 'aresapp';
+	
+	//Connect to the DB
+	$conn	= mysqli_connect($hostName, $userName, $pass, $database) or die('Connection error! (LOCAL)');
+	
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	
+	$query = "SELECT * FROM player WHERE playerName = '" . $username . "'";
+	
+	$result = mysqli_query($conn, $query);
+	
+	if(mysqli_num_rows($result) == 0){
+	
+		echo 'Incorrect Username or Password';
+		exit;
+	
+	}
+	else{
+		echo 'Found a record';
+	}
+	
+	
+?>
+</body>
