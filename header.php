25d5495c67bbd772270b8deca4e3dcbe51551ebf
jrdespai <jrdespai@asu.edu>
2015-03-12 21:38:02 -0700

    After meeting 3/12 - initial structure of myteam page

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+	echo '
+		<html lang="en">
+		 <head>
+			
+			<title>Ares App</title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+		
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+			<link rel="stylesheet" href="style/style.css">
+
+			<!-- jQuery library -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+			<!-- Latest compiled JavaScript -->
+			<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		
+			<!--<link rel="icon" href="images/logo.png" />-->
+		 </head>'
+?>
---
+++ team/myteam.php
@@ -0,0 +1,117 @@
+<!Doctype html>
+
+
+<!--<?php
+	include('header.php');
+?>-->
+
+
+		<html lang="en">
+		 <head>
+			
+			<title>Ares App</title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			
+		
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+			<link rel="stylesheet" href="style/style.css">
+
+			<!-- jQuery library -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+			<!-- Latest compiled JavaScript -->
+			<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		
+			<!--<link rel="icon" href="images/logo.png" />-->
+		 </head>
+
+  <body>
+	
+		<div class="container-fluid bg-warning">
+			<!--<img class="img-responsive img-rounded" src="images/logo.png" alt="FireBright Logo" />  need to learn how upload photo-->
+			<p>Team Name</p>
+		</div>
+
+		<div class="container">
+			<div class="col-10-sm">
+					<p>Soccer</p>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-5">
+						<p>Team Captain:</p>
+				</div>
+
+				<div class="col-sm-5">
+						<p>Kade Karges</p>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-5">
+						<p>Location:</p>
+				</div>
+
+				<div class="col-sm-5">
+						<p>Gilbert, AZ</p>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-5">
+						<p></p>
+				</div>
+
+				<div class="col-sm-5">
+						<p></p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Recent Games</th>
+						<th>Team Played</th>
+						<th>Win/Loss</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>3/15/15</td>
+						<td>Losers</td>
+						<td>win</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		
+		<div class="container">
+
+			<div class="row">
+				
+				<div class="col-sm-5">
+						<p>Progress:</p>
+				</div>
+
+				<div class="col-sm-5">
+						<p>50%</p>
+				</div>
+				
+				<div class="col-sm-5">
+						<p>Level:</p>
+				</div>
+
+				<div class="col-sm-5">
+						<p>150</p>
+				</div>
+				
+			</div>
+		</div>
+		
+  </body>
+  
+</html>
