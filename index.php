d6e9746bea7dd81ab6b1b1e460e5ac1a4e9268de
unknown <croliver@asu.edu>
2015-05-27 21:21:05 -0700

    css changes

--- index.php
+++ index.php
@@ -11,15 +11,25 @@
 	include('navbar.php');
 ?>
 
-  <body>
+ 
 		
-        <div class="container-fluid">
-				
-				<button class="btn"><a href="login.php">Sign in</a></button>
-				<button class="btn"><a href="register.php">Register</a></button>
+ <body id="welcomebody">
+		<div class="container">
+			
+					<div class="row">
+						<div class="col-md-6">
+							<a href="login.php"><button class="btn btnindex">Sign in</button></a>
+						</div>
+						<div class="col-md-6">
+							<a href="register.php"><button class="btn btnindex">Register</button></a>
+						</div>
+					</div>
+				</div>
 		</div>
+
 
   
   </body>
+
   
 </html>
